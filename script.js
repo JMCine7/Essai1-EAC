3e626ebff2016495bf5a47c12b2cbe789749c22e
JMCine7 <metheoreconsulting@gmail.com>
2024-06-29 05:17:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+<script>
+alert("hello world !")
+ 
+</script>
+
+consol.log("hello world !");
+console.log(" c'est maintenant");
+
+let myName = "John"
+console.log("Name", myName);
+myName="jplyn"
+console.log("Name", myName);
+
+const contry= "kamer";
